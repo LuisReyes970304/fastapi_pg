0d0de3d3a63ebf9575ec43c5dceff009aa33585d
LuisReyes970304 <luisrafaelreyescaro1@gmail.com>
2026-09-21 09:19:51 -0500

    feat: security problem fixed in login

--- api/routes/auth.py
+++ api/routes/auth.py
@@ -2,10 +2,12 @@ from fastapi import APIRouter, Depends
 from api.controller.auth_controller import AuthController
 from api.utils.session_util import get_session
 from sqlmodel import Session
+from api.dto.auth_dto import LoginModel
 
 router = APIRouter(prefix="/users", tags=["Auth"])
 auth_controller = AuthController()
 
 @router.post("/login/{email}")
-async def login(email: str, password: str, session: Session = Depends(get_session)):
-    return auth_controller.login(email, password, session)
+async def login(login_model: LoginModel, session: Session = Depends(get_session)):
+    user = auth_controller.login(login_model.email, login_model.password, session)
+    return user
